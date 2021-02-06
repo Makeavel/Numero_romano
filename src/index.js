0569a8c77bcca83202d945317565558cc7692da8
Makeavel <dpseutroco@gmail.com>
2021-02-06 14:50:03 -0300

    Nível fácil concluido

---
+++ src/index.js
@@ -0,0 +1,44 @@
+
+    var palavra = "ix" ;
+    console.log(`Entrada: ${palavra}`);
+    var entrada = palavra.split("");
+    
+
+    
+    let somaFinal = 0;
+    
+    for( let x=0 ; x < entrada.length ; x++){
+        
+        if ( entrada[x] == "i" || entrada[x] == "I"){
+            
+            if(entrada[x+1] != "V" || entrada[x+1] != "X"){
+                somaFinal += 1;
+            }
+            somaFinal -= 1;
+        }
+        
+        else if( entrada[x] == "v" || entrada[x] =="V" ){
+
+            if(entrada[x-1] != undefined && entrada[x-1] == "i"){
+                somaFinal += 4;     
+            }
+            else{
+                somaFinal += 5;
+            }
+        }
+
+        else if( entrada[x] == "x" || entrada[x] == "X"){
+
+            if(entrada[x-1] != undefined && entrada[x-1] == "i"){
+                somaFinal += 9;
+            }
+            else{
+                somaFinal += 10;
+            }
+        }
+       
+    } // for
+
+    console.log(`Resultado: ${somaFinal}`);
+    
+
