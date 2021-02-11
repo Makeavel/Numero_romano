7babfbad2f0b216bd5acaef837439342927e6acf
Makeavel <dpseutroco@gmail.com>
2021-02-11 10:45:33 -0300

    facil novo

--- Desafio/Facil2.js
+++ Desafio/Facil2.js
@@ -6,6 +6,7 @@ function entrada(palavra){
     var palavra ;
     console.log(`Entrada: ${palavra}`);
     const entrada = palavra.split("");
+    console.log(entrada);
     return translate(entrada);
 
 } 
@@ -15,10 +16,10 @@ function translate(entrada){
     let somaFinal = 0;
     
     for( let x=0 ; x < entrada.length ; x++){
-        
+        console.log(entrada);
         if ( entrada[x] == "i"){
             
-           entrada = ([undefined , "i"].includes(x-1)) ? somaFinal += 1 : somaFinal -= 1;
+           entrada = (["x" , "v"].includes(x + 1)) ? somaFinal -= 1 : somaFinal += 1;
         }
         
         else if( entrada[x] == "v" ){
