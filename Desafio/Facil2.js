79e9df454721f64aa5299e7809a137eff73bf122
Makeavel <dpseutroco@gmail.com>
2021-02-11 10:48:13 -0300

    reformulado

--- Desafio/Facil2.js
+++ Desafio/Facil2.js
@@ -6,7 +6,6 @@ function entrada(palavra){
     var palavra ;
     console.log(`Entrada: ${palavra}`);
     const entrada = palavra.split("");
-    console.log(entrada);
     return translate(entrada);
 
 } 
@@ -16,20 +15,20 @@ function translate(entrada){
     let somaFinal = 0;
     
     for( let x=0 ; x < entrada.length ; x++){
-        console.log(entrada);
+       
         if ( entrada[x] == "i"){
             
-           entrada = (["x" , "v"].includes(x + 1)) ? somaFinal -= 1 : somaFinal += 1;
+           entrada[x] = (["x" , "v"].includes(x + 1)) ? somaFinal += 1 : somaFinal -= 1;
         }
         
         else if( entrada[x] == "v" ){
 
-          entrada = ([undefined , "i"].includes(x-1)) ? somaFinal += 4 : somaFinal += 5;
+          entrada[x] = ([undefined , "i"].includes(x-1)) ? somaFinal += 4 : somaFinal += 5;
         }
 
         else if( entrada[x] == "x"){
   
-          entrada = ([undefined , "i"].includes(x-1)) ? somaFinal += 9 : somaFinal += 10;
+          entrada[x] = ([undefined , "i"].includes(x-1)) ? somaFinal += 9 : somaFinal += 10;
         }
             
     } // for
