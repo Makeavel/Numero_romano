0a191c84778ddc65427a9317cf72bf0109bdb197
Makeavel <dpseutroco@gmail.com>
2021-02-11 10:42:09 -0300

    Novo fácil

---
+++ Desafio/Facil2.js
@@ -0,0 +1,42 @@
+
+entrada("xix") ;
+
+function entrada(palavra){
+
+    var palavra ;
+    console.log(`Entrada: ${palavra}`);
+    const entrada = palavra.split("");
+    return translate(entrada);
+
+} 
+
+function translate(entrada){
+
+    let somaFinal = 0;
+    
+    for( let x=0 ; x < entrada.length ; x++){
+        
+        if ( entrada[x] == "i"){
+            
+           entrada = ([undefined , "i"].includes(x-1)) ? somaFinal += 1 : somaFinal -= 1;
+        }
+        
+        else if( entrada[x] == "v" ){
+
+          entrada = ([undefined , "i"].includes(x-1)) ? somaFinal += 4 : somaFinal += 5;
+        }
+
+        else if( entrada[x] == "x"){
+  
+          entrada = ([undefined , "i"].includes(x-1)) ? somaFinal += 9 : somaFinal += 10;
+        }
+            
+    } // for
+    console.log(`Resultado: ${somaFinal}`);
+}
+
+
+    
+
+    
+
